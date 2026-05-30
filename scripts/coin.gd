0c6ccbde36391aa2de367e28ca8e56fd96cb8531
Lavish Agrawal <lavishagrawal1820@gmail.com>
2026-05-30 15:23:13 +0530

    level 2 finished! (fixed bugs)

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -3,9 +3,10 @@ extends Area2D
 
 
 @onready var game_manager: Node = $"../../Game Manager"
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 func _on_body_entered(_body: Node2D) -> void:
 	
 	game_manager.add_score()
-	queue_free()
+	animation_player.play("pickup")
--- scripts/coins_two.gd
+++ scripts/coins_two.gd
@@ -1,8 +1,9 @@
 extends Area2D
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 
 func _on_body_entered(body: Node2D) -> void:
 	print("+1 Points!")
-	queue_free()
+	animation_player.play("pickup")
